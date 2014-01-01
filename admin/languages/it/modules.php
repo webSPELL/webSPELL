fb83052f98ca85543afd4d695f95624abf001a58
Manuel Zimmer <pyst4r@googlemail.com>
2014-01-01 09:38:59 +0100

    Added Modules Language File

---
+++ admin/languages/it/modules.php
@@ -0,0 +1,65 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2011 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'module_about'=>'Chi siamo',
+  'module_articles'=>'Articolo',
+  'module_awards'=>'Premi',
+  'module_calendar'=>'Calendario',
+  'module_challenge'=>'Sfida',
+  'module_clanwars'=>'Clanwars',
+  'module_contact'=>'Contatto',
+  'module_counter_stats'=>'Pagina Statistica',
+  'module_demos'=>'Demo',
+  'module_faq'=>'Domande frequenti',
+  'module_files'=>'File',
+  'module_forum'=>'Forum',
+  'module_gallery'=>'Galleria',
+  'module_guestbook'=>'Libro degli ospiti',
+  'module_history'=>'Storia',
+  'module_imprint'=>'Imprint',
+  'module_joinus'=>'Diventa un membro',
+  'module_links'=>'Collegamenti',
+  'module_linkus'=>'Link noa',
+  'module_members'=>'Membri',
+  'module_news'=>'News',
+  'module_newsletter'=>'Newsletter',
+  'module_polls'=>'Sondaggi',
+  'module_register'=>'Registrazione',
+  'module_registered_users'=>'Utente Registrato',
+  'module_search'=>'Cercare',
+  'module_server'=>'Server',
+  'module_shoutbox_content'=>'Shoutbox',
+  'module_sponsors'=>'Sponsori',
+  'module_squads'=>'Squadre',
+  'module_tags'=>'Etichetta',
+  'module_whoisonline'=>'Chi è online?'
+);
+?>
--- admin/settings.php
+++ admin/settings.php
@@ -26,6 +26,7 @@
 */
 
 $_language->read_module('settings');
+$_language->read_module('modules',true);
 
 if(!ispageadmin($userID) OR mb_substr(basename($_SERVER['REQUEST_URI']),0,15) != "admincenter.php") die($_language->module['access_denied']);
 
