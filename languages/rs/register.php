d42128fcd43135a7ecac8246c9946037c5954dcc
BVBPOWER <chrisbvb@t-online.de>
2014-02-22 03:15:14 +0100

    serbian language
    change .yu to .rs language file

---
+++ languages/rs/register.php
@@ -0,0 +1,63 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2010 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'activation_successful'=>'Ваш рачун је успјешно активиран.<br />Сада се можете пријавити на странице.',
+  'back'=>'Натраг',
+  'enter_nickname'=>'Молимо унесите ник.',
+  'enter_password'=>'Молимо унесите лозинку.',
+  'enter_username'=>'Молимо унесите корисничко име.',
+  'errors_there'=>'Појавиле су се неке грешке',
+  'for_login'=>'само за пријаву',
+  'invalid_mail'=>'Унијели сте неисправну е-маил адресу.',
+  'mail'=>'Е-Маил',
+  'mail_activation_successful'=>'Активација Ваше E-маил адресе је успјела.',
+  'mail_failed'=>'Активацијски е-маил није било могуће послати,молимо контактирајте вебмастера о овоме.',
+  'mail_inuse'=>'Наведена е-маил адреса се већ користи.',
+  'mail_subject'=>'Активацијски маил за рачун на %homepage_url%',
+  'mail_text'=>'Поздрав %username%!rnrnВаша регистрација на %pagetitle% (%homepage_url%) је  успјешна. Подаци за пријаву:rnrnКорисничко име: %username%rnЛозинка: %password%rnrnДа би завршили вашу регистацију,морате кликнути на активацијски линк:rn%activationlink%rnrnХвала Вам на регистацијиrn%pagetitle% - %homepage_url%',
+  'nickname'=>'Ник',
+  'nickname_inuse'=>'Ник већ користи неко други.',
+'no_register_when_loggedin'=>'You allready have an account at %pagename%',
+  'password'=>'Лозинка',
+  'profile_info'=>'Регистрацијске информације',
+  'register_now'=>'Региструј се!',
+  'register_successful'=>'Ваша регистрација је успјешна. Ускоро ћете примити е-маил са активацијским линком за Ваш рачун.',
+  'registration'=>'регистрација',
+  'repeat'=>'Понови лозинку',
+  'repeat_invalid'=>'Лозинке се не подударају.',
+  'security_code'=>'Сигурносни код',
+  'username'=>'Корисничко име',
+  'username_inuse'=>'Корисничко име неко већ користи.',
+  'username_toolong'=>'Корисничко име предуго (мах. 30 симбола).',
+  'wrong_activationkey'=>'Ваш активацијски кеј је погрешан!',
+  'wrong_securitycode'=>'Сигурносни код је погрешан!'
+);
+?>
---
+++ languages/rs/usergallery.php
@@ -0,0 +1,57 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2010 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'actions'=>'Акције',
+  'add_gallery'=>'додај галерију',
+  'add_img'=>'Додај имг',
+  'add_picture'=>'додај слику',
+  'comment'=>'Коментариши',
+  'delete'=>'Избриши',
+  'disable_comments'=>'Онемогући коментаре',
+  'edit'=>'Измјени',
+  'enable_user_comments'=>'Омогући коментаре корисницима',
+  'enable_visitor_comments'=>'Сви могу коментарисати',
+  'gallery_name'=>'Име галерије',
+  'group'=>'Група',
+  'name'=>'Име',
+  'new_gallery'=>'Нова галерија',
+  'no_galleries'=>'Нема доступни галерија',
+  'no_space_left'=>'Није вам преостало простора.',
+  'picture'=>'Слика',
+  'please_enter_name'=>'Молимо унесите име!',
+  'really_delete_gallery'=>'Cтварно избриши галерију?',
+  'update'=>'Спреми измјене',
+  'user_gallery'=>'Корисничка галерија',
+  'usergalleries'=>'Галерије корисника',
+  'visitor_comments'=>'Коментари посјетиоца',
+  'x_of_y_mb_in_use'=>'<span style="color:%spacecolor%">&nbsp;%used_size%</span> од %available_size% МБ се тренутно користи.'
+);
+?>
