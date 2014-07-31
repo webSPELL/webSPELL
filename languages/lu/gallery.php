2161857d2604d082200d86ea824b9e516857b7d2
BVBPOWER <chrisbvb@t-online.de>
2014-07-31 22:46:43 +0200

    Edit function + Add Luxembougish langauge
    Correct Luxemburg -> Luxembourg (english langauge)
    Add Luxembourgish language
    delete in function [delete "rs"], why should "rs" for serbia deleted,
    when we want to add it? Or am i'm wrong?

---
+++ languages/lu/gallery.php
@@ -0,0 +1,81 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2010 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+/**
+ * Online Language Editor Luxemburg language file for webSPELL 4.2.2
+ * @author Stefan Giesecke <webmaster@giesi.com>
+ * @copyright Copyright (c) 2010, Stefan Giesecke
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License v3
+ */
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'back'=>'Zréck',
+  'category'=>'Kategorie',
+  'comment'=>'Kommentar',
+  'comments'=>'Kommentaren',
+  'date'=>'Datum',
+  'delete'=>'läschen',
+  'edit'=>'Beaarbechten',
+  'edit_picture'=>'Bild beaarbechten',
+  'galleries'=>'Galerien',
+  'gallery'=>'Galerie',
+  'latest_gallery'=>'Lëscht Galerie',
+  'most_comments'=>'Meescht kommentéiert Galerie',
+  'name'=>'Numm',
+  'next'=>'Nächst',
+  'no_comments'=>'Kommentaren deaktivéieren',
+  'no_gallery_exists'=>'Keng Galerie verfügbar!',
+  'no_pic_set'=>'Keen Bild gesat',
+  'no_picture'=>'Keen Bild do',
+  'page_s'=>'Säite(n)',
+  'perfect'=>'Perfekt',
+  'pg_1_1'=>'Säite 1 von 1',
+  'picture'=>'Bild',
+  'pictures'=>'Biller',
+  'poor'=>'Schlecht',
+  'random_picture'=>'Zoufallsbild',
+  'rate'=>'bewäerten',
+  'rate_have_to_reg_login'=>'Bewäerten: Du musst registriert und angemeldet sein!',
+  'rate_now'=>'Jetzt bewäerten!',
+  'rating'=>'Bewäertung',
+  'really_del'=>'wierklech läschen?',
+  'reset_views'=>'Opruff zerécksetzen',
+  'show_galleries'=>'Galerien uweisen',
+  'start_diashow'=>'Diashow starten',
+  'update_picture'=>'Bild aktualiséieren',
+  'user_comments'=>'Benotzer-Kommentaren aktivéieren',
+  'usergalleries'=>'Benotzergalerien',
+  'views'=>'Opriff',
+  'visitor_comments'=>'Besischer-Kommentaren aktivéieren',
+  'votes'=>'Stëmmen',
+  'webs_diashow'=>'webSPELL Diashow:',
+  'you_have_already_rated'=>'Du hues schon bewert.',
+  'no_thumb'=>'Keen Thumb fonnt'
+);
+?>
