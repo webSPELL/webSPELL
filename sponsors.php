2a4aa38ffe57b3545c265acc511c723dbbef0406
René "Renegade" L. <xxxrenegade1990@gmail.com>
2014-11-23 00:13:34 +0100

    sponsors-module

--- sponsors.php
+++ sponsors.php
@@ -34,19 +34,17 @@ $ergebnis = safe_query("SELECT * FROM ".PREFIX."sponsors WHERE displayed = '1' O
 if(mysqli_num_rows($ergebnis)) {
 	$i = 1;
 	while($ds=mysqli_fetch_array($ergebnis)) {
-		if($i % 2) $bg1 = BG_1;
-		else $bg1 = BG_2;
 		
 		$url=str_replace('http://', '', $ds['url']);
-		$sponsor = '<a class="titlelink" href="out.php?sponsorID='.$ds['sponsorID'].'" target="_blank">'.$ds['name'].'</a>';
+		$sponsor = '<a href="out.php?sponsorID='.$ds['sponsorID'].'" target="_blank">'.$ds['name'].'</a>';
 		$link = '<a href="out.php?sponsorID='.$ds['sponsorID'].'" target="_blank">'.$url.'</a>';
 		$info = cleartext($ds['info']);
-		$banner = '<a href="out.php?sponsorID='.$ds['sponsorID'].'" target="_blank"><img src="images/sponsors/'.$ds['banner'].'" alt="'.htmlspecialchars($ds['name']).'" class="img-responsive center"></a>';
+		$banner = '<a href="out.php?sponsorID='.$ds['sponsorID'].'" target="_blank"><img src="images/sponsors/'.$ds['banner'].'" alt="'.htmlspecialchars($ds['name']).'" class="img-responsive"></a>';
 		
 		eval ("\$sponsors = \"".gettemplate("sponsors")."\";");
 		echo $sponsors;
 		$i++;
 	}
 }
-else echo $_language->module['no_sponsors'];
+else echo '<div class="alert alert-info" role="alert">'.$_language->module['no_sponsors'].'</div>';
 ?>
