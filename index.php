c57f8633fa73d586e45df5462c2bd2762bd11789
Philipp Helo Rehs <P.Rehs@gmx.net>
2014-12-07 15:23:27 +0100

    Removed the forced language information (en) from <html> tag, so the browser can detect the correct language

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $hide1 = ["forum", "forum_topic"];
 header('X-UA-Compatible: IE=edge,chrome=1');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
