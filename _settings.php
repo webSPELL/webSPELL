1e67b99e951d6ea83609695e82b8b5f5f27cce18
pascalmh <pascal@pascalmh.de>
2015-01-10 16:05:47 +0100

    refactor(php) replaced short array

--- _settings.php
+++ _settings.php
@@ -68,8 +68,10 @@ if (function_exists("globalskiller") == false) {
     function globalskiller()
     {
         // kills all non-system variables
-        $global =
-            ['GLOBALS', '_POST', '_GET', '_COOKIE', '_FILES', '_SERVER', '_ENV', '_REQUEST', '_SESSION', '_database'];
+        $global = array(
+            'GLOBALS', '_POST', '_GET', '_COOKIE', '_FILES', '_SERVER', '_ENV', '_REQUEST', '_SESSION', '_database'
+        );
+
         foreach ($GLOBALS as $key => $val) {
             if (!in_array($key, $global)) {
                 if (is_array($val)) {
