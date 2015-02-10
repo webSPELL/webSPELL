0390eda186697653f7470fd48700e6f553606d64
derchris <derchris@me.com>
2015-02-10 02:48:20 +0100

    chore(buildtools): implement beatfactor/nightwatch#135

--- tests/nightwatchjs/checkGoogleXpath.js
+++ tests/nightwatchjs/checkGoogleXpath.js
@@ -2,6 +2,7 @@
  * Created by derchris on 09/02/15.
  */
 
+var https = require('https');
 module.exports = {
     "Test Google" : function (browser) {
         browser
@@ -14,5 +15,47 @@ module.exports = {
             .pause(1000)
             .assert.containsText('//*[@id = "main"]', 'webSPELL.org CMS » Free Content Management System')
             .end();
+    },
+
+    tearDown : function(callback) {
+        var data = JSON.stringify({
+            "passed" : true,
+            "tags" : ["test","google"]
+        });
+
+        var requestPath = '/rest/v1/'+ this.client.options.username +'/jobs/' + this.client.sessionId;
+        try {
+            console.log('Updaing saucelabs', requestPath)
+            var req = https.request({
+                hostname: 'saucelabs.com',
+                path: requestPath,
+                method: 'PUT',
+                auth : this.client.options.username + ':' + this.client.options.access_key,
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Content-Length' : data.length
+                }
+            }, function(res) {
+                res.setEncoding('utf8');
+                console.log('Response: ', res.statusCode, JSON.stringify(res.headers));
+                res.on('data', function (chunk) {
+                    console.log('BODY: ' + chunk);
+                });
+                res.on('end', function () {
+                    console.info('Finished updating saucelabs.');
+                    callback();
+                });
+            });
+
+            req.on('error', function(e) {
+                console.log('problem with request: ' + e.message);
+            });
+            req.write(data);
+            req.end();
+        } catch (err) {
+            console.log('Error', err);
+            callback();
+        }
+
     }
 };
