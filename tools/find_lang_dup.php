10dd176b0277cb1776e8e72427559580566dc5d7
derchris <derchris@me.com>
2015-03-15 18:41:31 +0100

    chore(buildtools): fix cs

--- tools/find_lang_dup.php
+++ tools/find_lang_dup.php
@@ -19,39 +19,39 @@ $all_keys_site = '';
 $all_keys_admin = '';
 define('PAGETITLE', "PAGETITLE");
 
-foreach($lang_site as $lang) {
+foreach ($lang_site as $lang) {
     echo "Checking Site Language: " . $lang . " ... ", PHP_EOL;
     $files = glob('../languages/' . $lang . '/*');
     foreach ($files as $file) {
         $file_name = basename($file);
         $ext = substr($file_name, strrpos($file_name, "."));
         if ($ext == ".php") {
-            include_once($file);
+            include_once$file;
             $ref_keys_site[$file_name] = $language_array;
         }
     }
-    foreach($ref_keys_site[$file_name] as $key => $value){
+    foreach ($ref_keys_site[$file_name] as $key => $value) {
         $dup = array_search($key, $ref_keys_site[$file_name]);
-        if($key != $dup){
+        if ($key != $dup) {
             $duplicate_keys_site[$key] = $dup;
         }
     }
 }
 
-foreach($lang_admin as $lang) {
+foreach ($lang_admin as $lang) {
     echo "Checking Admin Language: " . $lang . " ... ", PHP_EOL;
     $files = glob('../admin/languages/' . $lang . '/*');
     foreach ($files as $file) {
         $file_name = basename($file);
         $ext = substr($file_name, strrpos($file_name, "."));
         if ($ext == ".php") {
-            include_once($file);
+            include_once$file;
             $ref_keys_admin[$file_name] = $language_array;
         }
     }
-    foreach($ref_keys_admin[$file_name] as $key => $value){
+    foreach ($ref_keys_admin[$file_name] as $key => $value) {
         $dup = array_search($key, $ref_keys_admin[$file_name]);
-        if($key != $dup){
+        if ($key != $dup) {
             $duplicate_keys_admin[$key] = $dup;
         }
     }
@@ -63,14 +63,12 @@ foreach($lang_admin as $lang) {
 }
 echo "", PHP_EOL;
 
-foreach($duplicate_keys_site as $key => $value) {
-    $all_keys_site .= $key. ", ";
+foreach ($duplicate_keys_site as $key => $value) {
+    $all_keys_site .= $key . ", ";
 }
-echo "Duplicate Language Keys in Site: " .$all_keys_site, PHP_EOL;
+echo "Duplicate Language Keys in Site: " . $all_keys_site, PHP_EOL;
 
-foreach($duplicate_keys_admin as $key => $value) {
-    $all_keys_admin .= $key. ", ";
+foreach ($duplicate_keys_admin as $key => $value) {
+    $all_keys_admin .= $key . ", ";
 }
-echo "Duplicate Language Keys in Admincenter: " .$all_keys_admin, PHP_EOL;
-
-
+echo "Duplicate Language Keys in Admincenter: " . $all_keys_admin, PHP_EOL;
