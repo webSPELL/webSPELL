df0d0cc3958b01fba5229c005bc6de37991fe41e
René Renegade Lammert <xxxrenegade1990@gmail.com>
2015-04-03 19:57:13 +0200

    style(index): add footer

--- index.php
+++ index.php
@@ -264,6 +264,12 @@ header('X-UA-Compatible: IE=edge,chrome=1');
         </div>
     </div>
 </div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">Copyright by <b><?php echo $myclanname ?></b>&nbsp; | &nbsp;CMS powered by <a href="http://www.webspell.org" target="_blank"><b>webSPELL.org</b></a>&nbsp; | &nbsp;<a href="http://validator.w3.org/check?uri=referer" target="_blank">HTML5</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS 3</a> valid W3C standards&nbsp; | &nbsp;<a href="tmp/rss.xml" target="_blank"><img src="images/icons/rss.png" width="16" height="16" style="vertical-align:bottom;" alt="" /></a> <a href="tmp/rss.xml" target="_blank">RSS Feed</a></p>
+    </div>
+</footer>
 <?php foreach ($components['js'] as $component) {
     echo '<script src="' . $component . '"></script>';
 }
