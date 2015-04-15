1e7d038063ab10ab4fd3e52903e1d8c8af5b7576
derchris <derchris@me.com>
2015-04-15 10:58:33 +0200

    chore(core): fix cs

--- _functions.php
+++ _functions.php
@@ -580,9 +580,8 @@ systeminc('func/email');
 
 function recursiveRemoveDirectory($directory)
 {
-    foreach(glob("{$directory}/*") as $file)
-    {
-        if(is_dir($file)) {
+    foreach (glob("{$directory}/*") as $file) {
+        if (is_dir($file)) {
             recursiveRemoveDirectory($file);
         } else {
             unlink($file);
