76272d958ee17f19d91d49e0bb58597f5ae1f995
derchris <derchris@me.com>
2015-04-22 01:58:29 +0200

    chore(core): remove obsolete function

--- _functions.php
+++ _functions.php
@@ -575,15 +575,3 @@ systeminc('func/feeds');
 // -- Email -- //
 
 systeminc('func/email');
-
-function recursiveRemoveDirectory($directory)
-{
-    foreach (glob("{$directory}/*") as $file) {
-        if (is_dir($file)) {
-            recursiveRemoveDirectory($file);
-        } else {
-            unlink($file);
-        }
-    }
-    rmdir($directory);
-}
