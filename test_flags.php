dfdeb1d80a10df031f27ef8956329810ba8b3c63
derchris <derchris@me.com>
2015-04-23 21:28:57 +0200

    chore(buildtools): add Crowdin Badge to README

---
+++ test_flags.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derchris
+ * Date: 22/04/15
+ * Time: 02:14
+ */
+
+echo generateFlags('select');
+echo generateFlags('dropdown');
+echo generateFlags('list');
