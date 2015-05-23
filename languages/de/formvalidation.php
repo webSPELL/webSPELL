cb94162d3f922688912b1eec6423ce2ef267bd5d
derchris <derchris@me.com>
2015-05-24 00:28:00 +0200

    chore(languages): add DE translation

---
+++ languages/de/formvalidation.php
@@ -0,0 +1,43 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2015 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+    'file_too_big'=>'Datei zu groß',
+    'incomplete_upload'=>'Unvollständiger Upload',
+    'no_file_uploaded'=>'Keine Datei hochgeladen',
+    'no_temp_folder_available'=>'Es stehen keine temporären Ordner zur Verfügung. Bitte wenden Sie sich an den Administrator',
+    'cant_write_temp_file'=>'Temporäre Datei kann nicht geschrieben werden. Bitte wenden Sie sich an den Administrator',
+    'unexpected_error'=>'Unerwarteter Fehler',
+    'cant_copy_file'=>'Datei kann nicht kopiert werden. Bitte wenden Sie sich an den Administrator',
+    'image_too_big'=>'Das Bild ist zu groß (max Größe %1$d px & %2$d px)',
+    'broken_image'=>'Die Bilddatei scheint kaputt zu sein',
+    'unsupported_image_type'=>'Nicht unterstützter Bildtyp',
+    'errors_there'=>'Es gibt Fehler',
+    'file_already_exists'=>'Datei existiert bereits'
+);
