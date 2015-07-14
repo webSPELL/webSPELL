36d9f9acf797cc76d90ca4791ac02d018e790889
Dieter Daems <daemsdieter@gmail.com>
2015-07-14 02:42:14 +0200

    Fix: Remove all BBcode to prevent breaking layout
    I decided to remove all BBcode, because the strlenghtfunction would cut of closing tags.
    Wich will in return than break the whole layout. I have no idea how to properly fix this. Maybe someone else can tackle this?
    Atm this does fix BBcodes being displayed on the webpage.

--- sc_topnews.php
+++ sc_topnews.php
@@ -52,13 +52,12 @@ if ($anz) {
 
     $headline = clearfromtags($message_array[ $showlang ][ 'headline' ]);
     $content = $message_array[ $showlang ][ 'message' ];
-    $content = nl2br(strip_tags($content));
+    $content = nl2br(strip_tags(htmloutput($content)));
 
     if (mb_strlen($content) > $maxtopnewschars) {
         $content = mb_substr($content, 0, $maxtopnewschars);
         $content .= '...';
     }
-    $content = htmloutput($content);
 
     $data_array = array();
     $data_array['$topnewsID'] = $topnewsID;
