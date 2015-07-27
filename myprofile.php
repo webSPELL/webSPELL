f9cdec5a24b0c7c6258c33f63dfd3e8cacf3e803
René Renegade Lammert <xxxrenegade1990@gmail.com>
2015-07-27 18:46:29 +0200

    fix(myprofile): col-**-** fix

--- myprofile.php
+++ myprofile.php
@@ -511,10 +511,10 @@ if (!$userID) {
             if ($user_guestbook) {
                 $user_gbook_select =
                     '<div class="form-group">
-                        <label for="select-user_guestbook" class="col-xs-12 col-md-3 control-label">' .
+                        <label for="select-user_guestbook" class="col-sm-3 control-label">' .
                         $_language->module['guestbook'] . ':</label>
 
-                        <div class="col-xs-12 col-md-9">
+                        <div class="col-sm-9">
                             <select name="user_guestbook" class="form-control" id="select-user_guestbook">
                                 ' . $user_gbook . '
                             </select>
